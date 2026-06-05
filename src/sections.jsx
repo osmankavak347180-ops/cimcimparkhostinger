245ec684d7535d47473b193f71c9c2da8be832e8
osmankavak347180-ops <osmankavak347180@gmail.com>
2026-06-05 14:59:13 +0300

    fix: hero slider gecis suresi 4s'den 6s'ye guncellendi

--- src/sections.jsx
+++ src/sections.jsx
@@ -189,7 +189,7 @@ function Hero() {
 
   useEffectS(() => {
     if (paused) return;
-    const id = setInterval(() => setActive((a) => (a + 1) % HERO_SLIDES.length), 4000);
+    const id = setInterval(() => setActive((a) => (a + 1) % HERO_SLIDES.length), 6000);
     return () => clearInterval(id);
   }, [paused]);
 
